6388ca28dd709a7e36317aa2216aac4324ff938b
Nick Garvey <garvey.nick@gmail.com>
2013-07-31 01:49:25 -0400

    Generalize some components of rendering and add lines

--- client/show-blocks.js
+++ client/show-blocks.js
@@ -1,5 +1,6 @@
 var TOTAL_SECTORS = 1953525168;
-var NUM_ROWS = 5;
+var NUM_ROWS = 10;
+var SPACE_BETWEEN_LINES = 10;
 
 function rgba(r, g, b, a) {
     this.r = r;
@@ -34,35 +35,54 @@ var clearRect = function(ctx, x, y, width, height, color) {
     };
 
     window.setTimeout(function() {
-        fadeTo(.5);
+        fadeTo(.25);
 
         window.setTimeout(function() {
-            fadeTo(.25);
+            fadeTo(.1);
 
             window.setTimeout(function() {
-                fadeTo(0);
+                fadeTo(.05);
 
-            }, 2000);
-        }, 2000);
-    }, 2000);
+                window.setTimeout(function() {
+                    fadeTo(.01);
+
+                    window.setTimeout(function() {
+                        fadeTo(0);
+
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
 
 };
 
 window.onload = function() {
     var canvas = document.getElementById('sda-div');
     canvas.width = window.innerWidth;
-    var canvasWidth = canvas.width;
+    canvas.height = window.innerHeight;
+
+    var lineHeight = Math.ceil(canvas.height / NUM_ROWS - SPACE_BETWEEN_LINES);
+
     var ctx = canvas.getContext("2d");
+
+    for (var rowNum = 0; rowNum < NUM_ROWS; rowNum++) {
+        var y = rowNum * (lineHeight + SPACE_BETWEEN_LINES);
+        ctx.fillRect(0, y, canvas.width, 1);
+
+        y = rowNum * (lineHeight + SPACE_BETWEEN_LINES) + lineHeight + 2;
+        ctx.fillRect(0, y, canvas.width, 1);
+    }
+
     socket.onmessage = function(msg) {
         var trace = JSON.parse(msg.data);
 
         var percentInDisk = trace.Sector / TOTAL_SECTORS;
         var rowNum = Math.floor(NUM_ROWS * percentInDisk);
 
-        var x = Math.floor(canvasWidth * (NUM_ROWS * percentInDisk - rowNum));
-        var y = rowNum * 150;
-        var width = Math.max(2, canvasWidth * trace.Blocks / TOTAL_SECTORS);
-        var height = 100;
+        var x = Math.floor(canvas.width * (NUM_ROWS * percentInDisk - rowNum));
+        var y = rowNum * (lineHeight + SPACE_BETWEEN_LINES) + 1
+        var width = Math.max(2, canvas.width * trace.Blocks / TOTAL_SECTORS);
 
         var color = new rgba(0, 0, 0, 1);
         if (trace.IoType == "W" || trace.IoType == "WS") {
@@ -71,12 +91,13 @@ window.onload = function() {
             color = new rgba(0, 255, 0, 1);
         } else {
             console.log("Unexpected type: " + trace.IoType);
+            return;
         }
 
         ctx.fillStyle = color.toString();
 
-        ctx.fillRect(x, y, width, height);
+        ctx.fillRect(x, y, width, lineHeight);
         
-        clearRect(ctx, x, y, width, height, color);
+        clearRect(ctx, x, y, width, lineHeight, color);
     };
 };
